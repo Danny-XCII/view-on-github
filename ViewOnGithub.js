4caa0a145bb24ca2f94604aebbf9aa560a23bbd6
Danny <daniel@winningwebdesign.co.uk>
2020-02-20 13:11:55 +0000

    added optional theme parameter, font colors updated

--- ViewOnGithub.js
+++ ViewOnGithub.js
@@ -1,10 +1,11 @@
 class ViewOnGithub {
 
-    constructor( el, username, project ) {
+    constructor( el, username, project, theme = "default" ) {
 
         this.el = document.getElementById( el );
         this.username = username;
         this.project = project;
+        this.theme = theme;
         this.uri = "https://github.com/" + this.username + "/" + this.project;
         this.insertCss();
         this.buildBar();
@@ -22,6 +23,12 @@ class ViewOnGithub {
 
         let head = document.querySelector( "head" );
         let html = "<link rel='stylesheet' type='text/css' href='https://tools.winningweb.co.uk/view-on-github/css/style.css'>";
+
+        if ( this.theme == "purple" ) {
+
+            html += "\n <style>.vog-bar { background-color: #8c7ae6; }<style>";
+
+        }
 
         head.insertAdjacentHTML( "beforeend", html );
 
